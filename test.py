3ca0ac063fc27516542642269e25b0e91418291e
scal-o <92513507+scal-o@users.noreply.github.com>
2023-05-08 20:33:39 +0200

    added multi-step simulations compatibility

--- test.py
+++ test.py
@@ -1,5 +1,4 @@
 import os, shutil, pathlib, re
-
 # script to test the functionality of run.py etc
 
 cwd = os.getcwd()
@@ -8,16 +7,18 @@ cwd = pathlib.Path(cwd)
 for obj in os.scandir(cwd):
     if obj.is_dir() and re.search("prima_copy", obj.name):
         shutil.rmtree(obj.path)
+        print("ciao")
 
 shutil.copytree(cwd / "sim_scripts" / "prima", cwd / "prima_copy")
-shutil.copytree(cwd / "sim_scripts" / "postProcessing", cwd / "prima_copy" / "postProcessing")
 
 os.mkdir(cwd / "prima_copy" / "results")
+
 
 for obj in os.scandir(cwd / "sim_scripts"):
     if obj.is_file() and re.search("\.py$", obj.name):
         shutil.copy(obj.path, cwd / "prima_copy")
 
-os.chdir(cwd / "prima_copy")
-run = cwd / "prima_copy" / "run.py"
-exec(open(run).read())
+# os.chdir(cwd / "prima_copy")
+# run = cwd /"prima_copy" / "run.py"
+
+# exec(open(run).read())
